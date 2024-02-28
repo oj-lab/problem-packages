0e5454a854f6324361c71e1eee0898af2af58670
Zztrans <2438362589@qq.com>
2024-02-28 15:38:09 +0800

    problem lin88-palindrome left

---
+++ icpc/lin88-palindrome/submissions/accepted/use_std.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef USE_STDIN
+#define USE_STDIN 0
+#endif
+
+class Solution {
+ public:
+  vector<string> lin88_palindrome(string s, int q, vector<int>& pos,
+                                  vector<char>& upd) {
+    int cnt = 0, n = s.size();
+    for (int i = 0; i < n / 2; ++i) {
+      if (s[i] != s[n - i - 1]) cnt++;
+    }
+    vector<string> ans;
+    for (int i = 0; i < q; ++i) {
+      char c = upd[i];
+      int p = pos[i] - 1;
+      if (s[p] == s[n - p - 1]) {
+        s[p] = c;
+        if (s[p] != s[n - p - 1]) cnt++;
+      } else {
+        s[p] = c;
+        if (s[p] == s[n - p - 1]) cnt--;
+      }
+      if (!cnt) {
+        ans.push_back("Yes");
+      } else {
+        ans.push_back("No");
+      }
+    }
+    return ans;
+  }
+};
+
+int main() {
+  Solution sol;
+  if (USE_STDIN) {
+    string s;
+    int q;
+    cin >> s >> q;
+    vector<int> pos(q);
+    vector<char> upd(q);
+    for (int i = 0; i < q; ++i) {
+      cin >> pos[i] >> upd[i];
+    }
+    // for (auto& it : upd) cout << it << endl;
+    vector<string> res = sol.lin88_palindrome(s, q, pos, upd);
+    for (auto& it : res) cout << it << endl;
+  } else {
+    string s = "abc";
+    int q = 3;
+    vector<int> pos = {1, 2, 3};
+    vector<char> upd = {'c', 'c', 'b'};
+    vector<string> res = sol.lin88_palindrome(s, q, pos, upd);
+    for (auto& it : res) cout << it << endl;
+  }
+  return 0;
+}
